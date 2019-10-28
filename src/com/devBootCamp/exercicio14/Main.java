8437c8abf268abf3fbd771ec3dd58e4300b29f7f
eltonmmoreira <eltonmmoreira@gmail.com>
2019-10-27 22:22:16 -0300

    Exercicio 14

---
+++ src/com/devBootCamp/exercicio14/Main.java
@@ -0,0 +1,60 @@
+package com.devBootCamp.exercicio14;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        var voo = new Voo(123, LocalDate.now(), LocalTime.of(23, 0));
+
+        loggerAssentosLivresEOcupados(voo);
+
+        loggerOcupado(voo, 10);
+
+        loggerOcuparAssento(voo, 15);
+        loggerOcupado(voo, 15);
+        loggerAssentosLivresEOcupados(voo);
+
+        loggerOcuparAssento(voo, 18);
+        loggerOcupado(voo, 18);
+        loggerAssentosLivresEOcupados(voo);
+
+        loggerOcuparAssento(voo, 34);
+        loggerOcupado(voo, 34);
+        loggerAssentosLivresEOcupados(voo);
+
+        loggerOcupado(voo, 34);
+
+        loggerOcuparAssento(voo, 1);
+        loggerOcupado(voo, 1);
+        loggerAssentosLivresEOcupados(voo);
+
+        var primeiroLivre = voo.primeiroLivre();
+        System.out.println("Primeiro livre: " +  primeiroLivre);
+
+        var proximoLivre = voo.proximoAssentoLivre(33);
+        System.out.println("Próximo assento livre: " + proximoLivre);
+        loggerAssentosLivresEOcupados(voo);
+    }
+
+    private static void loggerOcuparAssento(Voo voo, Integer assento) {
+        var ocupado = voo.ocuparAssento(assento);
+        System.out.println("Ocupar Assento: " + assento + " - " + ocupado);
+    }
+
+    private static void loggerOcupado(Voo voo, Integer assento) {
+        var ocupado = voo.verificaSeEstaOcupado(assento);
+        System.out.println("Assento Ocupado: " + assento + " - " + ocupado);
+    }
+
+    private static void loggerAssentosLivresEOcupados(Voo voo) {
+        System.out.println("Quantidade assentos livre: " + voo.quantidadeAssentosLivres());
+        System.out.println("Assentos livre: "
+                + voo.getAssentosLivres().stream().map(Object::toString).collect(Collectors.joining("-")));
+        System.out.println("Quantidade assentos ocupados: " + voo.getAssentosOcupados().size());
+        System.out.println("Assentos ocupados: "
+                + voo.getAssentosOcupados().stream().map(Object::toString).collect(Collectors.joining("-")));
+        System.out.println();
+    }
+}
---
+++ src/com/devBootCamp/exercicio14/Voo.java
@@ -0,0 +1,115 @@
+package com.devBootCamp.exercicio14;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Voo {
+    private Integer numeroVoo;
+    private LocalDate data;
+    private LocalTime hora;
+    private Integer numeroMaximoDeAssentos;
+    private List<Integer> assentosOcupados = new ArrayList<>();
+    private List<Integer> assentosLivres;
+
+    public Voo(Integer numeroVoo, LocalDate data, LocalTime hora) {
+        this.numeroVoo = numeroVoo;
+        this.data = data;
+        this.hora = hora;
+        numeroMaximoDeAssentos = 100;
+        assentosLivres = new ArrayList<>(numeroMaximoDeAssentos);
+        for (int i = 1; i <= numeroMaximoDeAssentos; i++) {
+            assentosLivres.add(i);
+        }
+    }
+
+    public Integer getNumeroVoo() {
+        return numeroVoo;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public LocalTime getHora() {
+        return hora;
+    }
+
+    public Integer getNumeroMaximoDeAssentos() {
+        return numeroMaximoDeAssentos;
+    }
+
+    public List<Integer> getAssentosOcupados() {
+        return assentosOcupados;
+    }
+
+    public Integer primeiroLivre() {
+        if (assentosLivres.isEmpty()) {
+            System.out.println("Nenhum assento livre");
+            return null;
+        }
+        assentosLivres.sort(Comparator.comparing(Integer::intValue));
+        return assentosLivres.get(0);
+    }
+
+    public Integer proximoAssentoLivre(Integer numeroAssento) {
+        if (assentosLivres.isEmpty()) {
+            System.out.println("Nenhum assento livre");
+            return null;
+        }
+
+        assentosLivres.sort(Comparator.comparing(Integer::intValue));
+        for (Integer assento : assentosLivres) {
+            if (assento > numeroAssento) {
+                return assento;
+            }
+        }
+
+//        return assentosLivres.stream().filter(a -> a >= numeroAssento)
+//                .findFirst()
+//                .orElse(null);
+        return null;
+    }
+
+    public boolean verificaSeEstaOcupado(Integer numeroAssento) {
+        for (Integer assento : assentosOcupados) {
+            if (numeroAssento.equals(assento)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean ocuparAssento(Integer numeroAssento) {
+        if (!verificaSeEstaOcupado(numeroAssento)) {
+            assentosLivres.remove(numeroAssento);
+            return assentosOcupados.add(numeroAssento);
+        }
+        return false;
+    }
+
+    public Integer quantidadeAssentosLivres() {
+        return numeroMaximoDeAssentos - assentosOcupados.size();
+    }
+
+    public List<Integer> getAssentosLivres() {
+        var assentosLivres = new ArrayList<Integer>();
+        for (int i = 1; i < numeroMaximoDeAssentos; i++) {
+            if (!assentosOcupados.contains(i)) {
+                assentosLivres.add(i);
+            }
+        }
+        return assentosLivres;
+        //return this.assentosLivres;
+    }
+
+    public Voo clone() {
+        var clone = new Voo(this.numeroVoo, this.data, this.hora);
+        clone.numeroMaximoDeAssentos = this.numeroMaximoDeAssentos;
+        clone.assentosLivres = this.assentosLivres;
+        clone.assentosOcupados = this.assentosOcupados;
+        return clone;
+    }
+}
